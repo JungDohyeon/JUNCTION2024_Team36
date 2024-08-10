67ce6042115753f6f93a2c4617301a912975ddfd
JungDohyeon <jdh1109ok@naver.com>
2024-08-10 16:32:19 +0900

    init

---
+++ smart_blinker/smart_blinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_blinkerApp.swift
+//  smart_blinker
+//
+//  Created by 정도현 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct smart_blinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
